c771a92a7dbd4ec3d8567f17c05bc9f7be46eb12
logikfabrik <anton@logikfabrik.se>
2016-08-26 10:31:30 +0200

    Style changes

--- src/Logikfabrik.Overseer.WPF.Provider.VSTeamServices/ViewModels/AddConnectionViewModel.cs
+++ src/Logikfabrik.Overseer.WPF.Provider.VSTeamServices/ViewModels/AddConnectionViewModel.cs
@@ -10,5 +10,47 @@ namespace Logikfabrik.Overseer.WPF.Provider.VSTeamServices.ViewModels
     /// <seealso cref="WPF.ViewModels.AddConnectionViewModel" />
     public class AddConnectionViewModel : WPF.ViewModels.AddConnectionViewModel
     {
+        private string _url = "https://";
+        private string _token;
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>
+        /// The URL.
+        /// </value>
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+
+            set
+            {
+                _url = value;
+                NotifyOfPropertyChange(() => Url);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        /// <value>
+        /// The token.
+        /// </value>
+        public string Token
+        {
+            get
+            {
+                return _token;
+            }
+
+            set
+            {
+                _token = value;
+                NotifyOfPropertyChange(() => Token);
+            }
+        }
     }
 }
