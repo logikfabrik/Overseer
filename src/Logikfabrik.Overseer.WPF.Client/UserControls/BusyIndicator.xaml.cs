020bcd0670e2d70cb0bd18d2c301d81bf1a0df19
logikfabrik <anton@logikfabrik.se>
2016-09-16 19:52:25 +0200

    Added busy indicator

---
+++ src/Logikfabrik.Overseer.WPF.Client/UserControls/BusyIndicator.xaml.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="BusyIndicator.xaml.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Client.UserControls
+{
+    /// <summary>
+    /// The <see cref="BusyIndicator" /> class.
+    /// </summary>
+    public partial class BusyIndicator
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BusyIndicator" /> class.
+        /// </summary>
+        public BusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Logikfabrik.Overseer.WPF/Converters/NullHiddenVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="NullHiddenVisibilityConverter.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// The <see cref="NullHiddenVisibilityConverter" /> class.
+    /// </summary>
+    public class NullHiddenVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <c>null</c>, the valid <c>null</c> value is used.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <c>null</c>, the valid <c>null</c> value is used.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
