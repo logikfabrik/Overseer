d3298648c642160990a34fe50bff466cba8f4128
logikfabrik <anton@logikfabrik.se>
2017-02-10 07:13:12 +0100

    Added project boilerplates

---
+++ src/Logikfabrik.Overseer.WPF.Provider.Jenkins/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logikfabrik.Overseer.WPF.Provider.Jenkins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Anton Frennevi")]
+[assembly: AssemblyProduct("Overseer")]
+[assembly: AssemblyCopyright("2016 Anton Frennevi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f4f07a30-be00-4295-bd36-3fc0b5e79b94")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
---
+++ src/Logikfabrik.Overseer.WPF.Provider.TeamCity/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logikfabrik.Overseer.WPF.Provider.TeamCity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Anton Frennevi")]
+[assembly: AssemblyProduct("Overseer")]
+[assembly: AssemblyCopyright("2016 Anton Frennevi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("89a4ea8b-685a-4a7f-91b3-6c16817fb5f6")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
