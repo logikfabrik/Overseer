f039bd9ff5163b57897dd242e34398013816e548
logikfabrik <anton@logikfabrik.se>
2017-05-11 17:17:14 +0200

    Added new provider CircleCI

---
+++ src/Logikfabrik.Overseer.WPF.Provider.CircleCI/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logikfabrik.Overseer.WPF.Provider.CircleCI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Anton Frennevi")]
+[assembly: AssemblyProduct("Overseer")]
+[assembly: AssemblyCopyright("2016 Anton Frennevi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("261d8616-8433-4c31-bb88-fc62fa389029")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
