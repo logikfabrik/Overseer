ea77c06ca86612e5189a1c85a2e9aba41330170c
logikfabrik <anton@logikfabrik.se>
2017-06-11 16:36:28 +0200

    Removed unneeded injection and added style for combo box

--- src/Logikfabrik.Overseer.WPF.Provider.AppVeyor/Module.cs
+++ src/Logikfabrik.Overseer.WPF.Provider.AppVeyor/Module.cs
@@ -24,7 +24,6 @@ namespace Logikfabrik.Overseer.WPF.Provider.AppVeyor
             Bind<IBuildProviderFactory>().To<BuildProviderFactory<ConnectionSettings, BuildProvider>>();
 
             Bind<WPF.ViewModels.IBuildProviderViewModel>().To<BuildProviderViewModel>();
-            Bind<Settings.ConnectionSettings>().To<ConnectionSettings>(); // TODO: Needed?
             Bind<WPF.ViewModels.EditConnectionViewModel<ConnectionSettings>>().To<EditConnectionViewModel>();
             Bind<WPF.ViewModels.Factories.IConnectionSettingsViewModelFactory<ConnectionSettings, ConnectionSettingsViewModel>>().To<WPF.ViewModels.Factories.ConnectionSettingsViewModelFactory<ConnectionSettings, ConnectionSettingsViewModel>>();
             Bind<WPF.ViewModels.Factories.IConnectionViewModelFactory>().To<WPF.ViewModels.Factories.ConnectionViewModelFactory<ConnectionSettings, WPF.ViewModels.ConnectionViewModel<ConnectionSettings>>>();
