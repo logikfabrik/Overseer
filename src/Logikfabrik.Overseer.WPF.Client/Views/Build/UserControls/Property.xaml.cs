9e5e4c1ae7d93b7506a2d276c9143cf55808d66d
logikfabrik <anton@logikfabrik.se>
2017-07-26 17:54:50 +0200

    Added control for build values

---
+++ src/Logikfabrik.Overseer.WPF.Client/Views/Build/UserControls/Property.xaml.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="Property.xaml.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Client.Views.Build.UserControls
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Media;
+
+    public partial class Property : INotifyPropertyChanged
+    {
+        public Property()
+        {
+            InitializeComponent();
+        }
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public bool HasValue => Value != null;
+
+        public object Value
+        {
+            get
+            {
+                return GetValue(ValueProperty);
+            }
+
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value", typeof(object), typeof(Property), new PropertyMetadata(null, PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var k = dependencyObject as Property;
+
+            k?.OnPropertyChanged("HasValue");
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+          "Icon", typeof(Geometry), typeof(Property));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
