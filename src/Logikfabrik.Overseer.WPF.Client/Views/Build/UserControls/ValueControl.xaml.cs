9acefe7dd055bd0b7e316bc98ebf02b75d56dbf5
logikfabrik <anton@logikfabrik.se>
2017-07-26 18:05:44 +0200

    Added missin files

---
+++ src/Logikfabrik.Overseer.WPF.Client/Views/Build/UserControls/ValueControl.xaml.cs
@@ -0,0 +1,101 @@
+﻿// <copyright file="ValueControl.xaml.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Client.Views.Build.UserControls
+{
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// The <see cref="ValueControl" /> class.
+    /// </summary>
+    public partial class ValueControl : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The icon dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon), typeof(Geometry), typeof(ValueControl));
+
+        /// <summary>
+        /// The value dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(object), typeof(ValueControl), new PropertyMetadata(null, PropertyChangedCallback));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValueControl" /> class.
+        /// </summary>
+        public ValueControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        /// <value>
+        /// The icon.
+        /// </value>
+        public Geometry Icon
+        {
+            get
+            {
+                return (Geometry)GetValue(IconProperty);
+            }
+
+            set
+            {
+                SetValue(IconProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public object Value
+        {
+            get
+            {
+                return GetValue(ValueProperty);
+            }
+
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has a value.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has a value; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasValue => Value != null;
+
+        /// <summary>
+        /// Called when a property is changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var control = dependencyObject as ValueControl;
+
+            control?.OnPropertyChanged(nameof(HasValue));
+        }
+    }
+}
