099c70c47285f11470b4de97cdd3f3dec04e0237
logikfabrik <anton@logikfabrik.se>
2017-08-13 11:42:29 +0200

    Working on the progress control

--- src/Logikfabrik.Overseer.WPF.Client/Views/UserControls/ProgressControl.xaml.cs
+++ src/Logikfabrik.Overseer.WPF.Client/Views/UserControls/ProgressControl.xaml.cs
@@ -1,28 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// <copyright file="ProgressControl.xaml.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
 
 namespace Logikfabrik.Overseer.WPF.Client.Views.UserControls
 {
+    using System.Windows;
+
     /// <summary>
-    /// Interaction logic for ProgressControl.xaml
+    /// The <see cref="ProgressControl" /> class.
     /// </summary>
-    public partial class ProgressControl : UserControl
+#pragma warning disable S110 // Inheritance tree of classes should not be too deep
+    public partial class ProgressControl
+#pragma warning restore S110 // Inheritance tree of classes should not be too deep
     {
+        /// <summary>
+        /// The is errored dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsErroredProperty = DependencyProperty.Register(nameof(IsErrored), typeof(bool), typeof(ProgressControl));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressControl" /> class.
+        /// </summary>
         public ProgressControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is errored.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is errored; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsErrored
+        {
+            get
+            {
+                return (bool)GetValue(IsErroredProperty);
+            }
+
+            set
+            {
+                SetValue(IsErroredProperty, value);
+            }
         }
     }
 }
