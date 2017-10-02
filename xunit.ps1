c439c847457b01b8f09d3265f8d5a9b8fa7042ba
logikfabrik <anton@logikfabrik.se>
2017-10-02 21:48:46 +0200

    Test config

--- xunit.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem $PSScriptRoot *.Test.dll -Recurse
-
-foreach ($file in $files) {
-    Write-Host $file.FullName
-    & "C:\Tools\xUnit20\xunit.console.x86.exe" $file.FullName -appveyor | Out-Host
-}
