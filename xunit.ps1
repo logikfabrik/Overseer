535583619f5eadc4ef56072dda84d35ba9dc14c8
logikfabrik <anton@logikfabrik.se>
2017-10-02 23:32:04 +0200

    Test config

--- xunit.ps1
+++ xunit.ps1
@@ -1,2 +1,1 @@
-Set-Location "C:\Tools\xUnit20\"
-& "xunit.console.x86.exe" $(Get-ChildItem $PSScriptRoot *.Test.dll -Recurse | % { $($_.FullName) }) -appveyor
+& "C:\Tools\xUnit20\xunit.console.x86.exe" $(Get-ChildItem $PSScriptRoot -r -i *.Test.dll | Where { $_.FullName -notlike "*\obj\*" } | % { $($_.FullName) }) -appveyor
