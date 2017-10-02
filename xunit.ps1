91cf57c971463174f9649797e13ed201eaf9e0a2
logikfabrik <anton@logikfabrik.se>
2017-10-02 22:26:59 +0200

    Test config

--- xunit.ps1
+++ xunit.ps1
@@ -1,1 +1,2 @@
-& "C:\Tools\xUnit20\xunit.console.x86.exe" $(Get-ChildItem $PSScriptRoot *.Test.dll -Recurse | % { $($_.FullName) }) -appveyor
+Set-Location "C:\Tools\xUnit20\"
+& "xunit.console.x86.exe" $(Get-ChildItem $PSScriptRoot *.Test.dll -Recurse | % { $($_.FullName) }) -appveyor
