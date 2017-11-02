bb80f7976544961a471b1444e46406e13bcb3f3b
logikfabrik <anton@logikfabrik.se>
2017-11-02 21:21:12 +0100

    Style changes

--- src/Logikfabrik.Overseer.WPF.Client/Properties/Resources.Designer.cs
+++ src/Logikfabrik.Overseer.WPF.Client/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Logikfabrik.Overseer.WPF.Client.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -206,7 +206,7 @@ namespace Logikfabrik.Overseer.WPF.Client.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Language.
+        ///   Looks up a localized string similar to LANGUAGE.
         /// </summary>
         public static string EditSettings_Setting_Label_CultureName {
             get {
@@ -215,7 +215,7 @@ namespace Logikfabrik.Overseer.WPF.Client.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Interval (s).
+        ///   Looks up a localized string similar to INTERVAL (s).
         /// </summary>
         public static string EditSettings_Setting_Label_Interval {
             get {
