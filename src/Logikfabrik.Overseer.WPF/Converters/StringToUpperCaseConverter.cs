bf8c5099ee1befddadcb6b7213340d97d9bfc28a
logikfabrik <anton@logikfabrik.se>
2017-11-04 11:41:57 +0100

    Style changes

---
+++ src/Logikfabrik.Overseer.WPF/Converters/StringToUpperCaseConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Logikfabrik.Overseer.WPF.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class StringToUpperCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as string;
+
+            return v?.ToUpper(culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
