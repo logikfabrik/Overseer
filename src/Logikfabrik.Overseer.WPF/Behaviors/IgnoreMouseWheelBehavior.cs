261a655d34a941393883f2d2d5ca94920920bfe7
logikfabrik <anton@logikfabrik.se>
2018-03-17 11:42:53 +0100

    Added behavior to ignore mouse wheel for password boxes, not to steal scroll

---
+++ src/Logikfabrik.Overseer.WPF/Behaviors/IgnoreMouseWheelBehavior.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="IgnoreMouseWheelBehavior.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Behaviors
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Interactivity;
+
+    /// <summary>
+    /// The <see cref="IgnoreMouseWheelBehavior" /> class.
+    /// </summary>
+    public class IgnoreMouseWheelBehavior : Behavior<PasswordBox>
+    {
+        /// <inheritdoc />
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewMouseWheel += AssociatedObject_PreviewMouseWheel;
+        }
+
+        /// <inheritdoc/>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (AssociatedObject == null)
+            {
+                return;
+            }
+
+            AssociatedObject.PreviewMouseWheel -= AssociatedObject_PreviewMouseWheel;
+        }
+
+        private void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+
+            AssociatedObject.RaiseEvent(new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent
+            });
+        }
+    }
+}
