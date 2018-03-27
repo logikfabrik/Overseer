9a4870efe6cc9da52ba9f223e45e85f91f1c4054
logikfabrik <anton@logikfabrik.se>
2018-03-27 12:14:00 +0200

    Layout and style changes

---
+++ src/Logikfabrik.Overseer.WPF/Styles/LayoutBreakpoint.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="LayoutBreakpoint.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Overseer.WPF.Styles
+{
+    /// <summary>
+    /// The <see cref="LayoutBreakpoint" /> class.
+    /// </summary>
+    public static class LayoutBreakpoint
+    {
+        /// <summary>
+        /// The medium layout breakpoint.
+        /// </summary>
+        public const int Medium = 650;
+
+        /// <summary>
+        /// The small layout breakpoint.
+        /// </summary>
+        public const int Small = 500;
+
+        /// <summary>
+        /// The extra small layout breakpoint.
+        /// </summary>
+        public const int ExtraSmall = 350;
+    }
+}
