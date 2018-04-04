b4875d6495fa77b079db66da6f6a5cc41f4588f2
logikfabrik <anton@logikfabrik.se>
2018-04-04 11:02:21 +0200

    Added comments

--- src/Logikfabrik.Overseer/IAppSettings.cs
+++ src/Logikfabrik.Overseer/IAppSettings.cs
@@ -25,12 +25,36 @@ namespace Logikfabrik.Overseer
         /// </value>
         bool ShowNotifications { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to show notifications for in progress builds.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if to show notifications for in progress builds; otherwise, <c>false</c>.
+        /// </value>
         bool ShowNotificationsForInProgressBuilds { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to show notifications for failed builds.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if to show notifications for failed builds; otherwise, <c>false</c>.
+        /// </value>
         bool ShowNotificationsForFailedBuilds { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to show notifications for succeeded builds.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if to show notifications for succeeded builds; otherwise, <c>false</c>.
+        /// </value>
         bool ShowNotificationsForSucceededBuilds { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to show notifications for stopped builds.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if to show notifications for stopped builds; otherwise, <c>false</c>.
+        /// </value>
         bool ShowNotificationsForStoppedBuilds { get; set; }
 
         /// <summary>
